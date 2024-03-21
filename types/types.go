60d1a6e29b5db7b4028f8671ba920063a97630a3
David Saah <dasorange.hope@gmail.com>
2024-03-21 14:45:17 +0000

    feat: define data types

---
+++ types/types.go
@@ -0,0 +1,15 @@
+package types
+
+// Person struct contains the fields for the person entity
+type People struct {
+	Passwd string
+	Fname  string
+	Lname  string
+	Email  string
+	Dob    string
+	Tel    string
+	Pid    uint
+	Rid    uint
+	Fid    uint
+	Gender uint
+}
