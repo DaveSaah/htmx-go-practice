cdad3f894c090561ef234d5122059d478e24f911
David Saah <dasorange.hope@gmail.com>
2024-03-21 14:45:30 +0000

    feat: create db connection

---
+++ db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// Init initializes a new connection to the database
+func Init() (*sql.DB, error) {
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "chores_mgt",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+		Loc:                  time.UTC,
+	}
+
+	return sql.Open("mysql", cfg.FormatDSN())
+}
